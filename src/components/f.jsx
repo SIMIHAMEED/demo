3c2c818aa59f8cbe128b6ab1cc3a60a5866cce7b
SIMIHAMEED <92150562+SIMIHAMEED@users.noreply.github.com>
2025-06-27 12:40:31 +0530

    Update f.jsx

--- src/components/f.jsx
+++ src/components/f.jsx
@@ -4,6 +4,7 @@ const f = () => {
   return (
     <div>
       <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores at quis cumque molestiae provident mollitia odit obcaecati reprehenderit. Qui, debitis. Magni dolores, dignissimos doloremque sint facilis tempore adipisci reprehenderit quisquam?</h1>
+      <h2> I am simi </h2>
     </div>
   )
 }
